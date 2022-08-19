537b2b8c864c606e813b8b69b74f030afc177120
unknown <jac.prz@live.com>
2022-08-19 20:48:28 +0200

    set up heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 
+const path = require('path')
 const dbConnect = require('./database/dbConnect');
 const blogRouter = require('./router/blogRoutes');
 const user = require('./router/user')
@@ -12,6 +13,13 @@ app.use('/login', user);
 
 
 dbConnect.mongoDbConnect();
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+} )
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`)
