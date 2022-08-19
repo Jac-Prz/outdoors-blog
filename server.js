0ae50abf76c1f9f17e6ef22578b79c2683cb2a23
unknown <jac.prz@live.com>
2022-08-19 21:16:41 +0200

    server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use('/login', user);
 
 dbConnect.mongoDbConnect();
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-} )
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
+    })
 }
 
 app.listen(process.env.PORT, () => {
